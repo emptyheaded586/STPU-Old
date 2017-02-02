42114cf40567781e8a4451de210568172754e1d8
kparliment <kparliment@daavlin.com>
2017-02-02 11:29:20 -0500

    All tables exporting to static location.

--- Export.cs
+++ Export.cs
@@ -18,13 +18,43 @@ namespace Smart_Touch_Protocol_Utility
                 {
                     connect.Open();
                     using (SqlDataReader reader = cmd.ExecuteReader())
-                    using (StreamWriter writer = new StreamWriter(@"c:\temp\Export\GlobalProtocols.csv"))
+                    using (StreamWriter writer = new StreamWriter(@"c:\temp\GlobalProtocols.csv"))
                     {
                         while (reader.Read())
                         {
                             writer.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}",
                               reader[0], reader[1], reader[2], reader[3], reader[4], 
                               reader[5], reader[6], reader[7], reader[8], reader[9]);
+                        }
+                    }
+                    cmd.CommandText = "SELECT * FROM [dbo].[GlobalProtocolTreatments]";
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    using (StreamWriter writer = new StreamWriter(@"c:\temp\GlobalProtocolTreatments.csv"))
+                    {
+                        while (reader.Read())
+                        {
+                            writer.WriteLine("{0},{1},{2},{3},{4}",
+                              reader[0], reader[1], reader[2], reader[3], reader[4]);
+                        }
+                    }
+                    cmd.CommandText = "SELECT * FROM [dbo].[UVATreatmentTypes]";
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    using (StreamWriter writer = new StreamWriter(@"c:\temp\UVATreatmentTypes.csv"))
+                    {
+                        while (reader.Read())
+                        {
+                            writer.WriteLine("{0},{1}",
+                              reader[0], reader[1]);
+                        }
+                    }
+                    cmd.CommandText = "SELECT * FROM [dbo].[UVBTreatmentTypes]";
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    using (StreamWriter writer = new StreamWriter(@"c:\temp\UVBTreatmentTypes.csv"))
+                    {
+                        while (reader.Read())
+                        {
+                            writer.WriteLine("{0},{1}",
+                              reader[0], reader[1]);
                         }
                     }
                 }
