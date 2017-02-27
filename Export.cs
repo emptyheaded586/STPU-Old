c5b511ded47908a4fea3cab1678cb8e4df0727c8
kparliment <kparliment@daavlin.com>
2017-02-27 11:34:22 -0500

    Column names are no longer hard coded in. Ask the user for location to save files.

--- Export.cs
+++ Export.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data.SqlClient;
 using System.IO;
+using System.Windows.Forms;
 
 namespace Smart_Touch_Protocol_Utility
 {
@@ -7,51 +8,101 @@ namespace Smart_Touch_Protocol_Utility
     {
         public static void exportTables()
         {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+
             using (SqlConnection connect = new SqlConnection(MainWindow.sqlConnection()))
             {
+                int numColumns;
                 string queryStatement = "SELECT * FROM [dbo].[GlobalProtocols]";
 
                 using (SqlCommand cmd = new SqlCommand(queryStatement, connect))
                 {
                     connect.Open();
                     using (SqlDataReader reader = cmd.ExecuteReader())
-                    using (StreamWriter writer = new StreamWriter(@"F:\GlobalProtocols.csv"))
+                    using (StreamWriter writer = new StreamWriter(fbd.SelectedPath + @"\GlobalProtocols.csv"))
                     {
-                        writer.WriteLine("GlobalProtocolID,MachineTypeCode,UVATreatmentTypeCode,UVBTreatmentTypeCode,SkinTypeListID,ScheduleListID,FYIDoseLimit,AuthDoseLimit,FYIDiff,AuthDiff");
+                        numColumns = reader.FieldCount;
+
+                        for (int x = 0; x < numColumns; ++x)
+                        {
+                            writer.Write(reader.GetName(x));
+                            if (x < (numColumns - 1))
+                            {
+                                writer.Write(",");
+                            }
+                        }
+                        writer.WriteLine();
                         while (reader.Read())
                         {
                             writer.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}",
-                              reader[0], reader[1], reader[2], reader[3], reader[4], 
+                              reader[0], reader[1], reader[2], reader[3], reader[4],
                               reader[5], reader[6], reader[7], reader[8], reader[9]);
                         }
                     }
+
                     cmd.CommandText = "SELECT * FROM [dbo].[GlobalProtocolTreatments]";
+
                     using (SqlDataReader reader = cmd.ExecuteReader())
-                    using (StreamWriter writer = new StreamWriter(@"F:\GlobalProtocolTreatments.csv"))
+                    using (StreamWriter writer = new StreamWriter(fbd.SelectedPath + @"\GlobalProtocolTreatments.csv"))
                     {
-                        writer.WriteLine("GlobalProtocolTreatmentID,GlobalProtocolID,TreatmentNumber,PrimaryDosage,SecondaryDosage");
+                        numColumns = reader.FieldCount;
+
+                        for (int x = 0; x < numColumns; ++x)
+                        {
+                            writer.Write(reader.GetName(x));
+                            if (x < (numColumns - 1))
+                            {
+                                writer.Write(",");
+                            }
+                        }
+                        writer.WriteLine();
                         while (reader.Read())
                         {
                             writer.WriteLine("{0},{1},{2},{3},{4}",
                               reader[0], reader[1], reader[2], reader[3], reader[4]);
                         }
                     }
+
                     cmd.CommandText = "SELECT * FROM [dbo].[UVATreatmentTypes]";
+
                     using (SqlDataReader reader = cmd.ExecuteReader())
-                    using (StreamWriter writer = new StreamWriter(@"F:\UVATreatmentTypes.csv"))
+                    using (StreamWriter writer = new StreamWriter(fbd.SelectedPath + @"\UVATreatmentTypes.csv"))
                     {
-                        writer.WriteLine("UVATreatmentTypeCode,UVATreatmentTypeDescription");
+                        numColumns = reader.FieldCount;
+
+                        for (int x = 0; x < numColumns; ++x)
+                        {
+                            writer.Write(reader.GetName(x));
+                            if (x < (numColumns - 1))
+                            {
+                                writer.Write(",");
+                            }
+                        }
+                        writer.WriteLine();
                         while (reader.Read())
                         {
                             writer.WriteLine("{0},{1}",
                               reader[0], reader[1]);
                         }
                     }
+
                     cmd.CommandText = "SELECT * FROM [dbo].[UVBTreatmentTypes]";
+
                     using (SqlDataReader reader = cmd.ExecuteReader())
-                    using (StreamWriter writer = new StreamWriter(@"F:\UVBTreatmentTypes.csv"))
+                    using (StreamWriter writer = new StreamWriter(fbd.SelectedPath + @"\UVBTreatmentTypes.csv"))
                     {
-                        writer.WriteLine("UVBTreatmentTypeCode,UVBTreatmentTypeDescription");
+                        numColumns = reader.FieldCount;
+
+                        for (int x = 0; x < numColumns; ++x)
+                        {
+                            writer.Write(reader.GetName(x));
+                            if (x < (numColumns - 1))
+                            {
+                                writer.Write(",");
+                            }
+                        }
+                        writer.WriteLine();
                         while (reader.Read())
                         {
                             writer.WriteLine("{0},{1}",
