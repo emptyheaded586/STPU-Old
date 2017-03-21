6b382dccc597961f9e3af5195f097cfe55221a43
kparliment <kparliment@daavlin.com>
2017-03-21 14:24:29 -0400

    DataTable name more descriptive, get & set for datatable.

--- DosageAmounts.xaml.cs
+++ DosageAmounts.xaml.cs
@@ -7,7 +7,7 @@ namespace Smart_Touch_Protocol_Utility
     public partial class DosageAmounts : Window
     {
         private int numTreat;
-        private static DataTable dt;
+        public static DataTable userDoses { get; private set; }
 
         public DosageAmounts()
         {
@@ -23,29 +23,24 @@ namespace Smart_Touch_Protocol_Utility
 
         private DataTable BindTable()
         {
-            dt = new DataTable();
-            dt.Columns.Add(new DataColumn("numTreat", Type.GetType("System.String")));
-            dt.Columns.Add(new DataColumn("doseAmt", Type.GetType("System.Double")));
+            userDoses = new DataTable();
+            userDoses.Columns.Add(new DataColumn("numTreat", Type.GetType("System.String")));
+            userDoses.Columns.Add(new DataColumn("doseAmt", Type.GetType("System.Double")));
 
             for (int x = 0; x < numTreat; ++x)
             {
                 DataRow row = null;
-                row = dt.NewRow();
+                row = userDoses.NewRow();
                 row["numTreat"] = "Treatment number # " + (x + 1);
                 row["doseAmt"] = 0;
-                dt.Rows.Add(row);
+                userDoses.Rows.Add(row);
             }
-            return dt;
+            return userDoses;
         }
 
         private void doneButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
-        }
-
-        public static DataTable getTable()
-        {
-            return dt;
         }
     }
 }
